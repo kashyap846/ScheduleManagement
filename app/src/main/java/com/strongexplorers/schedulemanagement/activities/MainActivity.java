d8bd796a5c8b021c3e3a852b18c081c9ab4911fe
Likhith <likhithp.s.@Likhiths-MacBook-Pro.local>
2020-06-02 20:38:37 -0400

    added manager screens

--- app/src/main/java/com/strongexplorers/schedulemanagement/activities/MainActivity.java
+++ app/src/main/java/com/strongexplorers/schedulemanagement/activities/MainActivity.java
@@ -1,8 +1,10 @@
-package com.strongexplorers.schedulemanagement;
+package com.strongexplorers.schedulemanagement.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.strongexplorers.schedulemanagement.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //setContentView(R.layout.activity_manager_home);
     }
 }
